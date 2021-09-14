e90c76df070decf8a5ad5fe7cfd0a40ae5379890
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-09-14 15:10:27 +0300

    Create AntiCheat.lua

---
+++ AntiCheat.lua
@@ -0,0 +1,154 @@
+local Players = game:GetService("Players")
+
+-- // If your game has admin commands which require noclipping, set this to false.
+local preventStrafingNoPhysics = true
+
+
+
+-- // Do not touch the code below unless you known what you are doing!
+
+local function protectHat(hat)
+	local handle = hat:WaitForChild("Handle", 30)
+
+	if handle then
+		task.defer(function()
+			local joint = handle:WaitForChild("AccessoryWeld")
+
+			local connection
+			connection = joint.AncestryChanged:Connect(function(_, parent)
+				if not connection.Connected or parent then
+					return
+				end
+
+				connection:Disconnect()
+
+				if handle and handle:CanSetNetworkOwnership() then
+					handle:SetNetworkOwner(nil)
+				end
+			end)
+		end)
+
+		if handle:IsA("Part") then
+			local mesh = handle:FindFirstChildOfClass("SpecialMesh") or handle:WaitForChild("Mesh")
+
+			mesh.AncestryChanged:Connect(function(child, parent)
+				task.defer(function()
+					if child == mesh and handle and (not parent or not handle:IsAncestorOf(mesh)) then
+						mesh.Parent = handle
+					end
+				end)
+			end)
+		end
+	end
+end
+
+local function killHumanoid(humanoid)
+	if humanoid then
+		humanoid:ChangeState(Enum.HumanoidStateType.Dead)
+		humanoid.Health = 0
+	end
+end
+
+Players.PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Connect(function(character)
+		for _, v in ipairs(character:GetChildren()) do
+			if v:IsA("Accoutrement") then
+				coroutine.wrap(protectHat)(v)
+			end
+		end
+
+		character.ChildAdded:Connect(function(child)
+			if child:IsA("Accoutrement") then
+				protectHat(child)
+			elseif child:IsA("BackpackItem") then
+				local count = 0
+
+				task.defer(function()
+					for _, v in ipairs(character:GetChildren()) do
+						if v:IsA("Tool") then
+							count += 1
+							if count > 1 then
+								v.Parent = player:FindFirstChildOfClass("Backpack") or Instance.new("Backpack", player)
+							end
+						end
+					end
+				end)
+			end
+		end)
+
+
+		local humanoid = character:FindFirstChildOfClass("Humanoid") or character:WaitForChild("Humanoid")
+
+		humanoid.AncestryChanged:Connect(function(child, parent)
+			task.defer(function()
+				if child == humanoid and character and (not parent or not character:IsAncestorOf(humanoid)) then
+					humanoid.Parent = character
+				end
+			end)
+		end)
+
+		humanoid.StateChanged:Connect(function(last, state)
+			if last == Enum.HumanoidStateType.Dead and state ~= Enum.HumanoidStateType.Dead then
+				killHumanoid(humanoid)
+			elseif preventStrafingNoPhysics and state == Enum.HumanoidStateType.StrafingNoPhysics then
+				killHumanoid(humanoid)
+			end
+		end)
+
+		do
+			local connection
+
+			connection = humanoid.Died:Connect(function()
+				if not connection.Connected then
+					return
+				end
+
+				connection:Disconnect()
+
+				task.wait(Players.RespawnTime + 1.5)
+
+				if workspace:IsAncestorOf(humanoid) then
+					player:LoadCharacter()
+				end
+			end)
+		end
+
+		local animator = humanoid:WaitForChild("Animator")
+
+		animator.AnimationPlayed:Connect(function(animationTrack)
+			local animationId = animationTrack.Animation.AnimationId
+			if animationId == "rbxassetid://148840371" or string.match(animationId, "[%d%l]+://[/%w%p%?=%-_%$&'%*%+%%]*148840371/*") then
+				killHumanoid(humanoid)
+			end
+		end)
+
+
+		local connections = {}
+		local function makeConnection(Conn)
+			local connection
+			connection = Conn:Connect(function(_, parent)
+				if not connection.Connected or parent then
+					return
+				end
+
+				for _, v in ipairs(connections) do
+					v:Disconnect()
+				end
+
+				if humanoid then
+					killHumanoid(humanoid)
+				end
+			end)
+
+			table.insert(connections, connection)
+		end
+
+		local rootJoint = humanoid.RigType == Enum.HumanoidRigType.R15 and character:WaitForChild("LowerTorso"):WaitForChild("Root") or humanoid.RigType == Enum.HumanoidRigType.R6 and character:WaitForChild("HumanoidRootPart"):WaitForChild("RootJoint")
+
+		makeConnection(rootJoint.AncestryChanged)
+
+		if humanoid.RigType == Enum.HumanoidRigType.R15 then
+			makeConnection(character:WaitForChild("UpperTorso"):WaitForChild("Waist").AncestryChanged)
+		end
+	end)
+end)
